e6892b8090025335ebcdd1acaa882efb3080faac
Kaustix <Kiernan.Lamarche@gmail.com>
2020-04-23 17:34:00 -0600

    changed database host variable

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -5,7 +5,7 @@ const configService = new ConfigService();
 
 const connection: ConnectionOptions = {
   type: configService.get<any>('DATABASE_TYPE'),
-  host:  configService.get<string>('DATABASE_URL'),
+  host:  configService.get<string>('DATABASE_HOST'),
   port:  configService.get<number>('DATABASE_PORT'),
   username: configService.get<string>('DATABASE_USER'),
   password: configService.get<string>('DATABASE_PASSWORD'),
