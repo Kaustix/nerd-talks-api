2b1dce440939affdf453dd2fcd6e4aba759d2a9f
Kaustix <Kiernan.Lamarche@gmail.com>
2020-04-24 11:25:48 -0600

    enabled cors and added helmet

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import * as helmet from 'helmet';
 import { AppModule } from './app.module';
 import { ConfigService } from './config.service';
 
@@ -13,6 +14,10 @@ async function bootstrap() {
       },
     }),
   );
+
+  app.use(helmet());
+  app.enableCors();
+
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT', 3000);
   const url = configService.get<string>('URL', '0.0.0.0');
